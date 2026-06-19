6390ba2b3d38af594e702f1158f1263e2a500ae5
hfizhirfan <hafizh.irfansyah41@gmail.com>
2026-06-19 09:58:11 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/HMI%20Putih.png' },
+        { rel: 'shortcut icon', type: 'image/png', href: '/HMI%20Putih.png' },
+      ],
+    },
+  },
 })
